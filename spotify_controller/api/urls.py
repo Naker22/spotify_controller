168d201b22ac556d2a6ad45b9a1644708eae3414
Stepan Tchynetskyi <60452439+Naker22@users.noreply.github.com>
2021-02-07 17:36:47 +0200

    final

--- spotify_controller/api/urls.py
+++ spotify_controller/api/urls.py
@@ -2,5 +2,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.main,name = 'main'),
+    path('', views.RoomView.as_view(),name = 'RoomView'),
+    path('create-room',views.CreateRoomView.as_view(),name = 'CreateRoom'),
+    path('get-room',views.GetRoom.as_view(),name = 'GetRoom'),
+    path('join-room',views.JoinRoom.as_view(),name = 'JoinRoom'),
+    path('user-in-room',views.UserInRoom.as_view(),name = 'UserIn'),
+    path('leave-room',views.LeaveRoom.as_view(),name = 'LeaveRoom'),
+    path('update-room',views.UpdateRoom.as_view(),name = 'Update'),
 ]
